61274cf4b5e1498e2370547e0e0b43761f045996
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 23:30:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|calls| calls.capitalize}
+  array.each {|calls| puts "#{calls}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  
+  while i < array.length
+  
 end
 
 def find_the_cheese# code an argument here
