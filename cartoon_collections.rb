ff7b3047b23f85e695ef0a9f105bee28a534c00e
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 23:34:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,6 @@ def long_planeteer_calls(array)
   end
 end
 
-
-  end
-end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
