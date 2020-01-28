050ab267b0a8a6d8d43c3e0f38e3ecbfd73c98bc
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 23:48:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,16 +14,14 @@ end
 
 def long_planeteer_calls(array)
   i = 0 
-  while i < array.length
-  array.each {|calls|
-    if calls.length > 4
-      true 
-    else
-      false
-    end }
-    i += 1 
+  if array.any? {|i| i.length > 4}
+    return true 
+  else
+    return false
+  i += 1 
   end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
