835bfc0c3ceb14ffdfbc6296ad7a855d6f0cf213
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-28 23:44:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(array)
   caps = []
   i = 0 
   while i < array.length
-  caps << array.map {|calls| calls.capitalize}
+  caps << array[i].capitalize
   caps.each {|calls| puts "#{calls}!"}
 end
 
