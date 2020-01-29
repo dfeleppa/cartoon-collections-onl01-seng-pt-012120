d0547c57151dd42173f908f3d3e4fe3bc209b348
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-29 01:23:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,13 @@ def long_planeteer_calls(array)
   end
 end
 
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0 
+  if array.any? {|i| i. > 4}
+    return true 
+  else
+    return false
+  i += 1 
+  end
 end
