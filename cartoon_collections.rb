3c5bf670da40640391c0b46e0759065635d90871
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-29 01:26:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
-  if array.any? {|i| i.include(cheese_types)}
+  if array.any? {|i| i.include?cheese_types)}
     return array[i]
   else
     return nil
